6ee00c24895033cd5c4906d23b36d021be221d31
Hester Knight <hester2knight@mac.com>
2020-10-04 20:23:05 +0100

    made two api calls and obtained token in local storage for authentication

--- src/Components/ResultList.js
+++ src/Components/ResultList.js
@@ -6,6 +6,47 @@ import SearchForm from "./SearchForm";
 export default function ResultList() {
   const classes = useStyles();
 
+  const [portfolioData, setPortfolioData] = useState(null);
+  const [taskData, setTaskData] = useState(null);
+
+  const token = localStorage.getItem("token");
+
+  const configPortfolio = {
+    method: "get",
+    url: "https://edocsapi.azurewebsites.net/Core6/api/Portfolio/ByUserId",
+    headers: {
+      Authorization: `Digest username="{{appToken}}" realm="_root_" password=${token}`,
+    },
+  };
+
+  const configTask = {
+    method: "get",
+    url: "https://edocsapi.azurewebsites.net/Core6/api/Tasks/ByUser",
+    headers: {
+      Authorization: `Digest username="{{appToken}}" realm="_root_" password=${token}`,
+    },
+  };
+
+  const requestOne = axios(configPortfolio);
+  const requestTwo = axios(configTask);
+
+  useEffect(() => {
+    axios
+      .all([requestOne, requestTwo])
+      .then(
+        axios.spread((...responses) => {
+          let responseOne = responses[0].data;
+          let responseTwo = responses[1].data;
+
+          setPortfolioData(responseOne);
+          setTaskData(responseTwo);
+        })
+      )
+      .catch((errors) => {
+        console.log(errors);
+      });
+  }, []);
+
   return (
     <div>
       <SearchForm />
