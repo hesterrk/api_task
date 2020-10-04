b3808752787dc66d4bea0a2d8df75e8553590f58
Hester Knight <hester2knight@mac.com>
2020-10-04 20:20:23 +0100

    created basic search form component to render on results list component

--- src/Components/ResultList.js
+++ src/Components/ResultList.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import axios from "axios";
+import SearchForm from "./SearchForm";
 
 export default function ResultList() {
   const classes = useStyles();
 
   return (
     <div>
+      <SearchForm />
       <h2 className={classes.header}> Your Results </h2>
     </div>
   );
---
+++ src/Components/SearchForm.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Styled from "styled-components";
+
+export default function SearchForm() {
+  return (
+    <Div>
+      <label htmlFor="enter">
+        Search For...
+        <Input id="enter" type="text" name="text"></Input>
+      </label>
+    </Div>
+  );
+}
+
+const Input = Styled.input`
+
+background: white;
+display: flex;
+justify-content: center;
+border: 3px #4776E6 solid;
+
+`;
+
+const Div = Styled.div`
+display: flex;
+justify-content: center;
+margin-top: 40px;
+margin-bottom: 20px;
+`;
