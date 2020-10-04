fcf5b1d093ea9f48d03e619e0791ad8345388c79
Hester Knight <hester2knight@mac.com>
2020-10-04 20:29:30 +0100

    displayed some of the data from tasks and portfolio calls and added some styling to the data container that holds them

--- src/Components/ResultList.js
+++ src/Components/ResultList.js
@@ -51,6 +51,50 @@ export default function ResultList() {
     <div>
       <SearchForm />
       <h2 className={classes.header}> Your Results </h2>
+
+      <div className={classes.dataContainer}>
+        <div>
+          {portfolioData
+            ? portfolioData.Result.sites.map((site, i) => {
+                return (
+                  <div>
+                    <p>
+                      <b> Site Name </b>: {site.name}
+                    </p>
+                    <div>
+                      {site.projects.map((proj) => {
+                        return (
+                          <p>
+                            <b>Project Name</b>: {proj.name}
+                          </p>
+                        );
+                      })}
+                    </div>
+                  </div>
+                );
+              })
+            : "nothing"}
+        </div>
+
+        <div>
+          {portfolioData
+            ? portfolioData.Result.sites[0].projects[0].documents.map(
+                (doc, i) => {
+                  return (
+                    <p>
+                      <b>Document Name </b>: {doc.name}
+                    </p>
+                  );
+                }
+              )
+            : "nothing "}
+        </div>
+
+        <div>
+          <b> Task Count </b> :
+          {taskData ? taskData.Result.tasks.length : "empty "}
+        </div>
+      </div>
     </div>
   );
 }
@@ -58,5 +102,15 @@ export default function ResultList() {
 const useStyles = makeStyles({
   header: {
     fontSize: "1rem",
+  },
+
+  dataContainer: {
+    border: "2px solid #928DAB",
+    margin: "0 auto",
+    padding: "20px",
+    borderRadius: "10px",
+    width: "75%",
+    boxShadow:
+      "0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)",
   },
 });
